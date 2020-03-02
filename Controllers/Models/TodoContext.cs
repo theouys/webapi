bbcd9aa9b1aa6381dae4664d62527da545ce6cc2
Theo Uys <theouys@outlook.com>
2020-03-02 21:00:37 +0200

    Start of project

---
+++ Controllers/Models/TodoContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace webapi1.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
---
+++ Controllers/Models/TodoItem.cs
@@ -0,0 +1,10 @@
+
+namespace webapi1.Models
+{
+        public class TodoItem
+        {
+            public long Id { get; set; }
+            public string Name { get; set; }
+            public bool IsComplete { get; set; }
+        }
+}
